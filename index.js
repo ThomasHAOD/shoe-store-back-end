b9afc8232c2da32c57b7af7eda28dd2978d0105e
TamHOD <thomashodonnell@gmail.com>
2020-01-22 11:57:53 +0000

    update server so cors exceptions allowed

--- index.js
+++ index.js
@@ -5,11 +5,29 @@ const port = 8000;
 const db = require("./queries");
 
 app.use(bodyParser.json());
+
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
 
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
