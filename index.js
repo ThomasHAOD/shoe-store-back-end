5f7cedef6acb283e792096a2d4d873bca8de1ad9
TamHOD <thomashodonnell@gmail.com>
2020-01-22 10:41:55 +0000

    add routes to access db, server running correctly with json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 8000;
+const db = require("./queries");
 
 app.use(bodyParser.json());
 app.use(
@@ -17,3 +18,6 @@ app.get("/", (request, response) => {
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
+
+app.get("/shoes", db.getShoes);
+app.get("/shoes/:id", db.getShoeById);
---
+++ queries.js
@@ -0,0 +1,33 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "Tam",
+  host: "localhost",
+  database: "shoe_store",
+  password: "password",
+  port: 5432
+});
+
+const getShoes = (request, response) => {
+  pool.query("SELECT * FROM shoes ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getShoeById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM shoes WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+module.exports = {
+  getShoes,
+  getShoeById
+};
